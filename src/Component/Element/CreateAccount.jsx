da2b18e0a82c46ee8ae85dedc0eada071ed450f1
aman201209 <mramankushwah30@gmail.com>
2024-08-03 11:57:14 +0530

    update

--- src/Component/Element/CreateAccount.jsx
+++ src/Component/Element/CreateAccount.jsx
@@ -2,29 +2,53 @@ import React, { useState } from 'react'
 import {app} from "../Firebase"
 import { getAuth,createUserWithEmailAndPassword } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
+import * as yup from "yup"
 
 const auth = getAuth(app)
 
 export default function CreateAccount() {
+
+    const uservalidation = yup.object({
+        email: yup.string().email('Invalid email').matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,"Invalid email format").required('Email is required'),
+        password: yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+      }); 
     let navigate = useNavigate()    
 
-    let [email , setEmail] = useState("")
-    let [password , setPassword] = useState("")
+    let [user , setUser] = useState({
+        email:"",
+        password:""
+    })
+    let [errors , setErrors] = useState({})
     
 
-    const handlesubmit=(e)=>{
+    const handlesubmit=async(e)=>{
         e.preventDefault();
-        createUserWithEmailAndPassword(auth , email , password)
-        .then((value)=>{
-            console.log(value.user.accessToken)
-            if(value.user.accessToken){
-                navigate("/point")
-            }
+        try {
+            await uservalidation.validate(user , {abortEarly:false})
+            setErrors({})
+            const {email , password} = user
+            createUserWithEmailAndPassword(auth , email , password)
+            .then((value)=>{
+                console.log(value.user.accessToken)
+                if(value.user.accessToken){
+                    navigate("/point")
+                }
+    
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
 
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
+        } catch (error) {
+            if(error){
+                const newErrors = {}
+                error.inner.forEach((err)=>{
+                    newErrors[err.path] = err.message
+                })
+                setErrors(newErrors)
+            }
+        }
+       
     }
 
   return (
@@ -35,10 +59,12 @@ export default function CreateAccount() {
     <div className='login1'>
         <form className='login3' onSubmit={handlesubmit}>
         <div>
-            <input type='email' placeholder='EMAIL'value={email} onChange={(e)=>setEmail(e.target.value)} />
+            <input type='email' placeholder='EMAIL'value={user.email} name='email' onChange={(e)=>setUser({...user,[e.target.name]:e.target.value})} />
+            {errors.email && <p className='error'>{errors.email}</p>}
         </div>
         <div>
-            <input type='password' placeholder='PASSWORD'value={password} onChange={(e)=>setPassword(e.target.value)}/>
+            <input type='password' placeholder='PASSWORD'value={user.password} name='password' onChange={(e)=>setUser({...user,[e.target.name]:e.target.value})}/>
+            {errors.password && <p className='error'>{errors.password}</p>}
         </div>
         <button className='button' type='submit'>SignUp</button>
         </form>
